300273b0f1d719a098b729a6e8359334d097af7c
ashok <ashokashi1997@gmail.com>
2023-11-07 16:49:05 +0530

    thank you

--- src/com/two/Two.java
+++ src/com/two/Two.java
@@ -1,10 +1,11 @@
 package com.two;
 
-public class Two {
+public class Two 
+{
 
 	public static void main(String[] args)
 	{
-		System.out.println(" Successfully commited to the gitHub,");
+		System.out.println(" Thank you naveen brother,");
 	}
 
 }
