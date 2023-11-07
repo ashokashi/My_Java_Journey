a4cfb170ca8c54c9b5f5cc085281eaee3b7bd449
ashok <ashokashi1997@gmail.com>
2023-11-07 16:42:09 +0530

    first commit

--- src/com/two/Two.java
+++ src/com/two/Two.java
@@ -2,9 +2,9 @@ package com.two;
 
 public class Two {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		System.out.println(" Successfully commited to the gitHub,");
 	}
 
 }
